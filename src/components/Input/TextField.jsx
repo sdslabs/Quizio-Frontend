b7f5771a2a6c09165e87b9aa5ca16c4aded95220
CapriciousRebel <code.capriciousrebel@gmail.com>
2022-01-11 05:03:04 +0530

    [Fix]: add react to scope for textfield

--- src/components/Input/TextField.jsx
+++ src/components/Input/TextField.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
 import PropTypes from 'prop-types';
 
 const TextField = ({
  id, label, error, inputProps,
 }) => (
     <div className="relative pt-5 w-full">
-        <label htmlFor={id} className="absolute top-0 text-sm text-grey-N6">{label}</label>
+        <label htmlFor={id} className="absolute top-0 text-sm text-grey-N6">
+            {label}
+        </label>
         <input
           id={id}
           {...inputProps}
@@ -13,26 +16,30 @@ const TextField = ({
           w-full text-sm placeholder-grey-N4::placeholder
           focus:outline-none focus:border-purple`}
         />
-        {error && <span className="text-sm text-red-error absolute right-0 top-0">{error}</span>}
+        {error && (
+        <span className="text-sm text-red-error absolute right-0 top-0">
+            {error}
+        </span>
+    )}
     </div>
 );
 
 TextField.propTypes = {
-    id: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
-    error: PropTypes.string,
-    inputProps: PropTypes.shape({
-        type: PropTypes.string.isRequired,
-        value: PropTypes.string,
-        onChange: PropTypes.func,
-        placeholder: PropTypes.string,
-        pattern: PropTypes.string,
-        disabled: PropTypes.bool,
-    }).isRequired,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  error: PropTypes.string,
+  inputProps: PropTypes.shape({
+    type: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
+    placeholder: PropTypes.string,
+    pattern: PropTypes.string,
+    disabled: PropTypes.bool,
+  }).isRequired,
 };
 
 TextField.defaultProps = {
-    error: '',
+  error: '',
 };
 
 export default TextField;
