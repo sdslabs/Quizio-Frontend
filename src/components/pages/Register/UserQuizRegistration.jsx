a9f7732408856d9c93a2642afc938c3417a2835a
pathange-s <sainatharao@gmail.com>
2022-01-18 23:45:34 +0530

    [Feature]: Implement basic flow of user registration form for quiz

---
+++ src/components/pages/Register/UserQuizRegistration.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import TextField from '@components/Input/TextField';
+import '@pagestyles/register/user_quiz_registration.scss';
+import PrimaryCTA from '@components/Buttons/PrimaryCTA';
+
+const UserQuizRegistration = () => {
+	const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+	const [emailID, setEmailID] = useState('');
+  const [contactNo, setContactNo] = useState('');
+  const [organisationName, setOrganisationName] = useState('');
+
+	return (
+    <div className="user-quiz-registration">
+        <div className="user-quiz-registration-title">Registration Form</div>
+        <div className="user-quiz-registration-basic-details">
+            <div className="user-quiz-registration-name">
+                <div className="user-quiz-registration-first-name">
+                    <TextField
+                      id="First name"
+                      placeholder="Candidate&#39;s name"
+                      label="First Name"
+                      error=""
+                      val={firstName}
+                      setVal={setFirstName}
+                    />
+                </div>
+                <div className="user-quiz-registration-last-name">
+                    <TextField
+                      id="Last name"
+                      placeholder="Candidate&#39;s name"
+                      label="Last Name"
+                      error=""
+                      val={lastName}
+                      setVal={setLastName}
+                    />
+                </div>
+            </div>
+            <div className="user-quiz-registration-contact">
+                <div className="user-quiz-registration-contact-email">
+                    <TextField
+                      id="Email"
+                      placeholder="Candidate&#39;s Email ID"
+                      label="Email"
+                      error=""
+                      val={emailID}
+                      setVal={setEmailID}
+                    />
+                </div>
+                <div className="user-quiz-registration-contact-contactno">
+                    <TextField
+                      id="Contact No."
+                      placeholder="Candidate&#39;s contact number"
+                      label="Contact No."
+                      error=""
+                      val={contactNo}
+                      setVal={setContactNo}
+                    />
+                </div>
+            </div>
+            <div className="user-quiz-registration-organisation-name">
+                <TextField
+                  id="Organisation Name"
+                  placeholder="Candidate&#39;s Organisation name"
+                  label="Organisation Name"
+                  error=""
+                  val={organisationName}
+                  setVal={setOrganisationName}
+                />
+            </div>
+        </div>
+        <div className="user-quiz-registration-additional-details-title">Additional Details</div>
+        <div className="user-quiz-registration-register-container">
+            <div>
+                <PrimaryCTA text="Register" onClick={console.log('todo')} />
+            </div>
+        </div>
+    </div>
+	);
+};
+
+export default UserQuizRegistration;
--- src/components/pages/Register/index.jsx
+++ src/components/pages/Register/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import UserQuizRegistration from './UserQuizRegistration';
 
 const index = () => (
     <div>
         Register!!!
+        <UserQuizRegistration />
     </div>
 	);
 
