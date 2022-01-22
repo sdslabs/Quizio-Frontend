a16e9fd08115d5fc26edb34afef72214f9729b3f
Rohith Varma Buddaraju <rohith.varma.buddaraju@gmail.com>
2022-01-23 02:43:49 +0530

    Added css to button and created new text field

--- src/components/Input/MarkdownTextField.jsx
+++ src/components/Input/MarkdownTextField.jsx
@@ -1,54 +1,75 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import TextField from '@components/Input/TextField';
 
 const MarkdownTextField = ({
-    id, label, placeholder, error, limit, val, setVal, onKeyDown,
+    id, placeholder, error, limit, val, setVal, onKeyDown,
 }) => {
     const [navState, setNavState] = useState('write');
+    const [currentLen, setCurrentLen] = useState(0);
+
+    const handleChange = (e) => {
+        const newVal = e.target.value;
+        if (newVal.length <= limit - 1 || limit === 0) {
+            setVal(newVal);
+        }
+        setCurrentLen(newVal.length);
+    };
 
     return (
         <div className="markdown">
             <button
               type="button"
-              onClick={() => {
-                setNavState('write');
-                }}
-              className={navState === 'preview'
-                  ? 'decoration-red-400'
-                  : 'decoration-blue-400'}
+              onClick={() => setNavState('write')}
+              className={navState === 'write'
+                  ? 'px-4 py-0 border-purple-600 border-b-2 text-purple-600'
+                  : 'px-4 py-0 border-gray-400 border-b-2 text-gray-400'}
             >
                 Write
             </button>
             <button
               type="button"
-              onClick={() => {
-                    setNavState('preview');
-                    }}
-              className={navState === 'write'
-                  ? 'decoration-red-400'
-                  : 'decoration-blue-400'}
+              onClick={() => setNavState('preview')}
+              className={navState === 'preview'
+                  ? 'px-4 py-0 border-purple-600 border-b-2 text-purple-600'
+                  : 'px-4 py-0 border-gray-400 border-b-2 text-gray-400'}
+
             >
                 Preview
             </button>
 
-            <TextField
-              id={id}
-              label={label}
-              placeholder={placeholder}
-              error={error}
-              limit={limit}
-              val={val}
-              setVal={setVal}
-              onKeyDown={onKeyDown}
-            />
+            <div className="relative pt-5 w-full">
+
+                <input
+                  value={val}
+                  onChange={handleChange}
+                  onKeyDown={onKeyDown}
+                  placeholder={placeholder}
+                  id={id}
+                  className={`mt-1 p-4 border border-${
+                        error ? 'red-error' : 'grey-N4'
+                    } rounded
+                    w-full text-sm placeholder-grey-N4::placeholder
+                    focus:outline-none focus:border-purple`}
+                />
+                {error && (
+                <span className="text-sm text-red-error absolute right-0 top-0">
+                    {error}
+                </span>
+                )}
+                {limit !== 0 && (
+                <div className="flex w-full justify-end text-sm text-grey-N6">
+                    {currentLen}
+                    /
+                    {limit}
+                </div>
+                )}
+            </div>
         </div>
     );
 };
 
 MarkdownTextField.propTypes = {
     id: PropTypes.string.isRequired,
-    label: PropTypes.string,
     placeholder: PropTypes.string.isRequired,
     error: PropTypes.string,
     limit: PropTypes.number,
@@ -59,7 +80,6 @@ MarkdownTextField.propTypes = {
 
 MarkdownTextField.defaultProps = {
     error: '',
-    label: '',
     limit: 0,
     onKeyDown: () => {},
 };
--- src/components/pages/Landing/index.jsx
+++ src/components/pages/Landing/index.jsx
@@ -1,33 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Dashboard from '@pages/Dashboard';
 import JoinUs from '@pages/JoinUs';
-import { checkAuth } from '@api/auth';
-import { useDispatch } from 'react-redux';
-import { setUser } from '@redux/actions/auth';
+// import { checkAuth } from '@api/auth';
+// import { useDispatch } from 'react-redux';
+// import { setUser } from '@redux/actions/auth';
 import MarkdownTextField from '@components/Input/MarkdownTextField';
 
 function Landing() {
-  const dispatch = useDispatch();
-  const [isLoading, setIsLoading] = useState(true);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const dispatch = useDispatch();
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(async () => {
-    // try to login using existing jwt Token
-    const checkAuthRes = await checkAuth();
-    if (checkAuthRes.success) {
-      dispatch(setUser(checkAuthRes.data.user));
-      setIsLoggedIn(true);
-    } else {
-      dispatch(setUser({}));
-      setIsLoggedIn(false);
-    }
-    setIsLoading(false);
-  }, []);
+  // useEffect(async () => {
+  //   // try to login using existing jwt Token
+  //   const checkAuthRes = await checkAuth();
+  //   if (checkAuthRes.success) {
+  //     dispatch(setUser(checkAuthRes.data.user));
+  //     setIsLoggedIn(true);
+  //   } else {
+  //     dispatch(setUser({}));
+  //     setIsLoggedIn(false);
+  //   }
+  //   setIsLoading(false);
+  // }, []);
 
+  const [example, setExample] = useState('');
+  const isLoading = true;
+  const isLoggedIn = false;
   return (
       <>
           {isLoading ? (
-              <MarkdownTextField />
+              <MarkdownTextField
+                id="quiz name"
+                placeholder="Enter quiz name"
+                label="Quiz Name"
+                error=""
+                limit={20}
+                val={example}
+                setVal={setExample}
+              />
       ) : (
           <>{isLoggedIn ? <Dashboard /> : <JoinUs />}</>
       )}
