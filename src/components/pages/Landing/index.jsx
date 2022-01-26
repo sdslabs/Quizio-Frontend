ab19c4a2849de7fcda4e559de3270f9d9e943d40
pathange-s <sainatharao@gmail.com>
2022-01-26 23:28:41 +0530

    [Feature]: Integrate backend Oauth with frontend

--- src/components/pages/Landing/index.jsx
+++ src/components/pages/Landing/index.jsx
@@ -1,41 +1,41 @@
-import React from 'react';
-// import Dashboard from '@pages/Dashboard';
+import React, { useState, useEffect } from 'react';
+import Dashboard from '@pages/Dashboard';
 import JoinUs from '@pages/JoinUs';
-// import { checkAuth } from '@api/auth';
-// import { useDispatch } from 'react-redux';
-// import { setUser } from '@redux/actions/auth';
+import { checkAuth } from '@api/auth';
+import { useDispatch } from 'react-redux';
+import { setUser } from '@redux/actions/auth';
+// import { check } from 'prettier';
 
 function Landing() {
-  // const dispatch = useDispatch();
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const dispatch = useDispatch();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // useEffect(async () => {
-  //   // try to login using existing jwt Token
-  //   const checkAuthRes = await checkAuth();
-  //   if (checkAuthRes.success) {
-  //     dispatch(setUser(checkAuthRes.data.user));
-  //     setIsLoggedIn(true);
-  //   } else {
-  //     dispatch(setUser({}));
-  //     setIsLoggedIn(false);
-  //   }
-  //   setIsLoading(false);
-  // }, []);
+  useEffect(async () => {
+    const queryParams = new URLSearchParams(window.location.search);
+    const jwtToken = queryParams.get('jwtToken');
 
-  // return (
-  //     <>
-  //         {isLoading ? (
-  //             <>
-  //                 <JoinUs />
-  //             </>
-  //     ) : (
-  //         <>{isLoggedIn ? <Dashboard /> : <JoinUs />}</>
-  //     )}
-  //     </>
-  // );
-    return (
-        <JoinUs />
-    );
+    if (jwtToken == null) {
+      dispatch(setUser({}));
+      setIsLoggedIn(false);
+    } else {
+      const checkAuthRes = await checkAuth(jwtToken);
+      if (checkAuthRes.success) {
+        // TODO : if user is new user | show register  page
+
+        // else show dashboard
+        dispatch(setUser(checkAuthRes.data.user));
+        setIsLoggedIn(true);
+      } else {
+        dispatch(setUser({}));
+        setIsLoggedIn(false);
+      }
+    }
+  }, []);
+
+  return (
+      <div>
+          <>{isLoggedIn ? <Dashboard /> : <JoinUs />}</>
+      </div>
+  );
 }
 export default Landing;
