e181c3767324f600c958bed4438a3df9bbb06ad1
Saurabh <saurabhsangam78@gmail.com>
2022-01-26 12:29:43 +0530

    feat: add zustand and react-query

--- src/components/pages/GiveQuiz/QuizLanding.jsx
+++ src/components/pages/GiveQuiz/QuizLanding.jsx
@@ -4,10 +4,13 @@ import PrimaryCTA from '@components/Buttons/PrimaryCTA';
 import ModalWrapper from '@components/Modals/ModalWrapper';
 // import UserQuizRegistration from '@pages/Register/UserQuizRegistration';
 import StartQuizModal from '@pages/Register/StartQuizModal';
+import useSampleStore from '../../../stores/sample';
 
 const QuizLanding = () => {
     const { quizId } = useParams();
-    console.log(quizId);
+    const sampleStore = useSampleStore();
+
+    console.log({ sampleStore, quizId });
 
     const [showModal, setShowModal] = useState(false);
 
