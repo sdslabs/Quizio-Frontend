741743aa2990f6f819986e79062b0581fb433d51
pathange-s <sainatharao@gmail.com>
2022-01-26 23:43:11 +0530

    [Feature]: Add SDSLabs icon in OAuth modal

--- src/components/Buttons/OAuthButton.jsx
+++ src/components/Buttons/OAuthButton.jsx
@@ -4,12 +4,13 @@ import '@styles/base/buttons.scss';
 import { AiOutlineArrowRight } from 'react-icons/ai';
 import { BsGithub } from 'react-icons/bs';
 import { FcGoogle } from 'react-icons/fc';
+import sdslabsIcon from '@icons/sdslabsIcon.png';
 
 const OAuthButton = ({ label, id, additionalClassName }) => (
     <div className="o-auth-button-div">
-        {id === 'github' ? <BsGithub className="o-auth-icon" size={25} /> : <></>}
-        {id === 'google' ? <FcGoogle className="o-auth-icon" size={25} /> : <></>}
-        {id === 'sdslabs' ? <BsGithub className="o-auth-icon" size={25} /> : <></>}
+        {id === 'github' ? <BsGithub className="o-auth-icon" size={28} /> : <></>}
+        {id === 'google' ? <FcGoogle className="o-auth-icon" size={28} /> : <></>}
+        {id === 'sdslabs' ? <img src={sdslabsIcon} alt="Logo" className="w-9" /> : <></>}
         <button type="button" className={`o-auth-button ${additionalClassName} `}>
             {label}
         </button>
