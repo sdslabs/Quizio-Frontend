9521d39b461bd90b5259c37a1e3f71d173c2beca
CapriciousRebel <code.capriciousrebel@gmail.com>
2022-01-26 12:54:06 +0530

    [Feature]: Define routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from '@pages/Landing';
-import Login from '@pages/Login';
 import Register from '@pages/Register';
 import CreateQuiz from '@pages/CreateQuiz';
 import GiveQuiz from '@pages/GiveQuiz/index';
@@ -11,12 +10,27 @@ import './index.css';
 const App = () => (
     <Router>
         <Switch>
+            {/** NOT Logged IN */}
+            {/* Join Us page (Landing) */}
             <Route exact path="/" component={Landing} />
-            <Route exact path="/login" component={Login} />
+            {/* Public profile page */}
+            <Route path="/profile/:profileID" component={Landing} />
+
+            {/** Logged IN */}
+            {/* Dashboard page */}
+            <Route exact path="/" component={Dashboard} />
+            {/* Registration Page */}
             <Route exact path="/register" component={Register} />
+            {/* Create or edit a quiz */}
             <Route exact path="/quiz/create" component={CreateQuiz} />
-            <Route path="/quiz" component={GiveQuiz} />
-            <Route path="/dashboard" component={Dashboard} />
+            {/* Create or edit a quiz */}
+            <Route exact path="/quiz/edit" component={CreateQuiz} />
+            {/* Check a quiz */}
+            <Route path="/quiz/check/:quizID" component={CreateQuiz} />
+            {/* Attempt a quiz */}
+            <Route path="/quiz/:quizID" component={GiveQuiz} />
+            {/* Private profile page */}
+            <Route path="/profile/:profileID" component={Landing} />
         </Switch>
     </Router>
 );
