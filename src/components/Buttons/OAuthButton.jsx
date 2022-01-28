6f81baac7cf620d318b268c3cc40951f1fd18685
pathange-s <sainatharao@gmail.com>
2022-01-28 17:49:27 +0530

    [Feature]: Use {icons}.svg instead of react-icons

--- src/components/Buttons/OAuthButton.jsx
+++ src/components/Buttons/OAuthButton.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { AiOutlineArrowRight } from 'react-icons/ai';
-import { BsGithub } from 'react-icons/bs';
-import { FcGoogle } from 'react-icons/fc';
-import sdslabsIcon from '@icons/sdslabsIcon.png'; // TODO: Use SVG
+import arrowRight from '@icons/arrowRight.svg';
+import github from '@icons/githubIcon.svg';
+import google from '@icons/googleIcon.svg';
+import sdslabs from '@icons/sdslabsIcon.svg';
 import '@styles/base/buttons.scss';
 
 const OAuthButton = ({ label, id, additionalClassName }) => (
     <div className="o-auth-button-div">
-        {id === 'github' ? <BsGithub className="o-auth-icon" size={28} /> : <></>}
-        {id === 'google' ? <FcGoogle className="o-auth-icon" size={28} /> : <></>}
-        {id === 'sdslabs' ? <img src={sdslabsIcon} alt="Logo" className="w-9" /> : <></>}
+        {id === 'github' ? <img src={github} alt="github" className="w-9" /> : <></>}
+        {id === 'google' ? <img src={google} alt="google" className="w-9" /> : <></>}
+        {id === 'sdslabs' ? <img src={sdslabs} alt="sdslabs" className="w-9" /> : <></>}
         <button type="button" className={`o-auth-button ${additionalClassName} `}>
             {label}
         </button>
-        <AiOutlineArrowRight className="o-auth-arrow-icon" />
+        <img src={arrowRight} alt="->" className="o-auth-arrow-icon" />
     </div>
 );
 OAuthButton.propTypes = {
--- src/components/pages/Register/index.jsx
+++ src/components/pages/Register/index.jsx
@@ -7,7 +7,7 @@ import Step3 from '@images/register-step3.svg';
 const Register = () => {
   // const [step, setStep] = useState('1');
   // setStep('2');
-  const step = '3';
+  const step = '1';
   return (
       <div className="register-page">
           <div className="register-page-left-section">
@@ -16,7 +16,6 @@ const Register = () => {
                   {step === '2' ? 'Take a deep breath !' : ''}
                   {step === '3' ? 'Getting Started' : ''}
               </div>
-              {/* <img src={step === '1' ? Step1 : <>{step === '2' ? Step2 : Step3}</>} alt="Register - Step1" /> */}
               <>{step === '1' ? <img src={Step1} alt="Register - Step1" /> : ''}</>
               <>{step === '2' ? <img src={Step2} alt="Register - Step2" /> : ''}</>
               <>{step === '3' ? <img src={Step3} alt="Register - Step3" /> : ''}</>
