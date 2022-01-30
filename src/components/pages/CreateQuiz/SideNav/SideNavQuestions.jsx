e9ca1ebafc353f42fa5d5b5d379105b0c134d285
diya-pancholi <76246591+diya-pancholi@users.noreply.github.com>
2022-01-30 12:07:01 +0530

    [WIP]add section flow (#39)
    * add section flow

--- src/components/pages/CreateQuiz/SideNav/SideNavQuestions.jsx
+++ src/components/pages/CreateQuiz/SideNav/SideNavQuestions.jsx
@@ -2,35 +2,111 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { ReactComponent as QuestionsIcon } from '@icons/CreateQuiz/SideNavIcons/questions.svg';
-// import { ReactComponent as QuestionsSelectedIcon } from '@icons/CreateQuiz/SideNavIcons/questionsSelected.svg';
+import { ReactComponent as QuestionsSelectedIcon } from '@icons/CreateQuiz/SideNavIcons/questionsSelected.svg';
 import { ReactComponent as DropdownArrowDownIcon } from '@icons/dropdownArrowDown.svg';
 import { ReactComponent as DropdownArrowUpIcon } from '@icons/dropdownArrowUp.svg';
 import { setCreateQuizStage } from '@redux/actions/quiz';
+import QuestionBubble from '@pages/GiveQuiz/QuestionBubble';
+import PlusIcon from '@icons/plusIcon.svg';
 
+const sectionsArr = [];
 const SideNavQuestions = ({ selected }) => {
   const dispatch = useDispatch();
   const [active, setActive] = useState(false);
+  const [activeNav, setActiveNav] = useState('');
+  const [sections, setSections] = useState(sectionsArr);
+  const [clicked, setClicked] = useState(false);
 
+  const addNewSection = () => {
+    const newSection = {
+      label: `Section ${sections.length + 1}`,
+      count: sections.length + 1,
+      questions: [],
+    };
+    setSections([...sections, newSection]);
+  };
+  const addNewQuestion = (count) => {
+    const newSection = [...sections];
+    const questionNumber = newSection[count - 1].questions.length + 1;
+    newSection[count - 1].questions.push(questionNumber);
+    setSections(newSection);
+  };
   useEffect(() => {
     setActive(selected === 'Questions');
   }, [selected]);
-
   return (
-      <button
-        type="button"
-        onClick={() => {
-        dispatch(setCreateQuizStage('Questions'));
-      }}
-        className="create-quiz-sidenav-question"
-      >
-          <div className="icon">
-              <QuestionsIcon />
+      <div className="flex flex-col">
+
+          <button
+            type="button"
+            onClick={() => {
+          setClicked(!clicked);
+          dispatch(setCreateQuizStage('Questions'));
+        }}
+            className={
+          selected === 'Questions'
+            ? 'create-quiz-sidenav-option-selected '
+            : 'create-quiz-sidenav-option '
+        }
+          >
+              <div className="create-quiz-sidenav-option-icon">
+                  {selected === 'Questions' ? <QuestionsSelectedIcon /> : <QuestionsIcon />}
+              </div>
+              <div className={
+          selected === 'Questions'
+            ? 'create-quiz-sidenav-option-text-selected'
+            : 'create-quiz-sidenav-option-text'
+        }
+              >
+                  Questions
+
+              </div>
+              <div className="dropdown">
+                  {active ? <DropdownArrowUpIcon /> : <DropdownArrowDownIcon />}
+              </div>
+          </button>
+          {clicked && (
+          <div>
+              {' '}
+
+              {active && sections.map(({ label, count, questions }) => (
+                  <>
+                      <p
+                        className={`side-nav-item${activeNav === label ? '-active' : ''} flex justify-between`}
+                        onClick={() => setActiveNav(label)}
+                      >
+                          {label}
+                          <DropdownArrowDownIcon />
+                      </p>
+                      {activeNav
+                && (
+                <div>
+                    <div className={`side-nav-questions${activeNav === label ? '-active' : ''}`}>
+                        {questions.map((question) => (
+                            <QuestionBubble number={question} key={question} type="not-visited" />
+                      ))}
+                        <div>
+                            <img src={PlusIcon} alt="" onClick={() => addNewQuestion(count)} />
+                        </div>
+                    </div>
+
+                </div>
+                )}
+                  </>
+          ))}
+              <div className="p-4">
+                  <button
+                    type="button"
+                    className="side-nav-item-active w-full "
+                    onClick={addNewSection}
+                  >
+                      + Add Section
+                  </button>
+              </div>
           </div>
-          <div className="text">Questions</div>
-          <div className="dropdown">
-              {active ? <DropdownArrowUpIcon /> : <DropdownArrowDownIcon />}
-          </div>
-      </button>
+      )}
+      </div>
+
   );
 };
 SideNavQuestions.propTypes = {
--- src/components/pages/CreateQuiz/index.jsx
+++ src/components/pages/CreateQuiz/index.jsx
@@ -4,19 +4,14 @@ import SideNav from './SideNav';
 import MainForm from './MainForm';
 import '@pagestyles/create_quiz/index.scss';
 
-const CreateQuiz = () => {
-  const a = 1;
-  console.log(a);
-
-  return (
-      <div className="create-quiz">
-          <Navbar />
-          <div className="create-quiz-main">
-              <SideNav />
-              <MainForm />
-          </div>
-      </div>
-  );
-};
+const CreateQuiz = () => (
+    <div className="create-quiz">
+        <Navbar />
+        <div className="create-quiz-main">
+            <SideNav />
+            <MainForm />
+        </div>
+    </div>
+);
 
 export default CreateQuiz;
