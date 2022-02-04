b39045fa82d8cada9e11b389b7ce62f8dab3a625
Rohith Varma Buddaraju <rohith.varma.buddaraju@gmail.com>
2022-02-05 00:55:13 +0530

    Added icons to profile page

--- src/components/pages/ProfilePage/index.jsx
+++ src/components/pages/ProfilePage/index.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import gmail from '@icons/gmail.svg';
+import phone from '@icons/phone.svg';
+import facebook from '@icons/facebook.svg';
+import instagram from '@icons/instagram.svg';
+import linkedin from '@icons/linkedin.svg';
 
 // const [attemptedQuizzes, setAttemptedQuizzes] = useState([]);
 // const [hostedQuizzes, setHostedQuizzes] = useState([]);
@@ -11,7 +16,6 @@ const ProfilePage = () => (
         <div className="mx-40 pt-32">
             <div className="flex flex-row justify-evenly">
                 <ProfileCard />
-                I
                 <div className="h-52 w-40 rounded bg-purple-V1 flex flex-col justify-center items-center">
                     <div className="text-6xl font-semibold text-purple-V6 pb-4">5</div>
                     <div className="text-xl text-center text-purple-V6">Attempted</div>
@@ -36,7 +40,7 @@ const ProfilePage = () => (
     </div>
 );
 const QuizCard = () => (
-    <div className="relative w-full h-36 my-2 rounded border border-solid border-purple-V6">
+    <div className="relative w-full h-36 my-2 rounded border border-solid border-purple-V6 border-opacity-60">
         <div className="flex flex-row">
             <div className="p-4">
                 <img src="./blackbird.jpg" className="h-28 w-28 object-cover rounded" alt="QuizImage" />
@@ -79,8 +83,19 @@ const ProfileCard = () => (
                 <div className="text-2xl text-purple-V6 font-bold pt-3">Somesh Solanki</div>
                 <div className="text-black-1">Indian Institute of Technology</div>
                 <div className="text-purple-V6">Bangalore</div>
-                <div className="pt-4">john.doe@gmail.com</div>
-                <div className="text-black-1">+91 9876543210</div>
+                <div className="pt-4 gap-x-2 flex flex-row">
+                    <img src={gmail} className="h-4 w-5" alt="Gmail" />
+                    <div className="text-sm text-black-1">john.doe@gmail.com</div>
+                </div>
+                <div className="pt-1 flex gap-x-2 flex-row">
+                    <img src={phone} className="h-4 w-5" alt="Phone" />
+                    <div className="text-sm text-black-1">9980929292</div>
+                </div>
+            </div>
+            <div className="flex flex-col gap-y-6 px-8 mt-12">
+                <img src={facebook} className="h-6 w-6" alt="Facebook" />
+                <img src={instagram} className="h-6 w-6" alt="Instagram" />
+                <img src={linkedin} className="h-6 w-6" alt="Linkedin" />
             </div>
         </div>
     </div>
