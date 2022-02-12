ba9fbdbe924910bdc7e57d715887ab2cc431c075
Angad Kambli <angadkambli@gmail.com>
2022-02-12 21:39:23 +0530

    add quiz checking table sorting and pagination

--- src/components/pages/CheckQuiz/Bottom/CheckingTable.jsx
+++ src/components/pages/CheckQuiz/Bottom/CheckingTable.jsx
@@ -1,45 +1,82 @@
-import React, {} from 'react';
+import React, { useState } from 'react';
 import '@pagestyles/check_quiz/checking_table.scss';
 import PropTypes from 'prop-types';
 import CheckingProgress from './CheckingProgress';
 
-const CheckingTable = ({ data }) => (
-    <div className="checking-table-container">
-        <table>
-            <tbody>
-                <tr>
-                    <th className="text-left table-content">Sr.</th>
-                    <th className="text-left table-content">Name</th>
-                    <th className="table-content">Current rank</th>
-                    <th className="table-content">Current marks</th>
-                    <th className="table-content">Checking progress</th>
-                    <th className="table-content">Check quiz</th>
-                </tr>
-                {
-                        data.map((participant) => (
-                            <tr>
-                                <td className="text-left table-content">{participant.sr_num}</td>
-                                <td className="text-left table-content table-link">{participant.name}</td>
-                                <td className="text-center table-content">{participant.rank}</td>
-                                <td className="text-center table-content">{participant.marks}</td>
-                                <td className="text-center table-content"><CheckingProgress progress={participant.progress} /></td>
-                                <td className="text-center table-content table-link">Check Quiz</td>
+const MAX_PAGE_SIZE = 5;
+const CheckingTable = ({ data }) => {
+    const [pageNum, setPageNum] = useState(0);
+    return (
+        <div className="checking-table-container">
+            <table>
+                <tbody>
+                    <tr>
+                        <th className="text-left table-content">Sr.</th>
+                        <th className="text-left table-content">Name</th>
+                        <th className="table-content">Current rank</th>
+                        <th className="table-content">Current marks</th>
+                        <th className="table-content">Checking progress</th>
+                        <th className="table-content">Check quiz</th>
+                    </tr>
+                    {data
+                        .filter(
+                            (val, index) => index >= MAX_PAGE_SIZE * pageNum
+                                && index < MAX_PAGE_SIZE * (pageNum + 1),
+                        )
+                        .map((participant) => (
+                            <tr key={participant.sr_num}>
+                                <td className="text-left table-content">
+                                    {participant.sr_num}
+                                </td>
+                                <td className="text-left table-content table-link">
+                                    {participant.name}
+                                </td>
+                                <td className="text-center table-content">
+                                    {participant.rank}
+                                </td>
+                                <td className="text-center table-content">
+                                    {participant.marks}
+                                </td>
+                                <td className="text-center table-content">
+                                    <CheckingProgress
+                                      progress={participant.progress}
+                                    />
+                                </td>
+                                <td className="text-center table-content table-link">
+                                    Check Quiz
+                                </td>
                             </tr>
-                            ))
-                    }
-            </tbody>
-        </table>
-        <div className="page-cta-flex">
-            <span className="page-cta">Previous</span>
-            <span className="page-cta">Next</span>
+                        ))}
+                </tbody>
+            </table>
+            <div className="page-cta-flex">
+                <span
+                  role="button"
+                  tabIndex={0}
+                  className="page-cta"
+                  onClick={() => {
+                        if (pageNum > 0) setPageNum(pageNum - 1);
+                    }}
+                >
+                    Previous
+                </span>
+                <span
+                  role="button"
+                  tabIndex={0}
+                  className="page-cta"
+                  onClick={() => {
+                        if ((pageNum + 1) * MAX_PAGE_SIZE < data.length) { setPageNum(pageNum + 1); }
+                    }}
+                >
+                    Next
+                </span>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 CheckingTable.propTypes = {
-    data: PropTypes.arrayOf(
-        PropTypes.object,
-    ).isRequired,
-  };
+    data: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default CheckingTable;
--- src/components/pages/CheckQuiz/Bottom/index.jsx
+++ src/components/pages/CheckQuiz/Bottom/index.jsx
@@ -5,23 +5,75 @@ import SecondaryCTA from '@components/Buttons/SecondaryCTA';
 import { useGetRegistrants } from '@api/register/useRegister';
 import CheckingTable from './CheckingTable';
 
+const SORT_TYPES = {
+    CHECKED_ASC: 'Checked (0 - 100%)',
+    CHECKED_DES: 'Checked (100 - 0%)',
+    ALPHA_ASC: 'Alphabetic (A - Z)',
+    ALPHA_DES: 'Alphabetic (Z - A)',
+    RANKLIST: 'Ranklist',
+};
 const Bottom = () => {
     const search = new URLSearchParams(window.location.search);
     const quizID = search.get('quizID');
     const [tableData, setTableData] = useState([]);
-    const { data: registrantsData, isLoading: isRegistrantsLoading, isSuccess: isRegistrantsSuccess } = useGetRegistrants(quizID);
+    const [sortBy, setSortBy] = useState(0);
+    const {
+        data: registrantsData,
+        isLoading: isRegistrantsLoading,
+        isSuccess: isRegistrantsSuccess,
+    } = useGetRegistrants(quizID);
     useEffect(() => {
         if (isRegistrantsSuccess) {
-            setTableData(registrantsData.data.data.users.map((val, index) => ({
-                sr_num: index,
-                name: val,
-                rank: Math.floor(Math.random() * 50),
-                marks: Math.floor(Math.random() * 50),
-                progress: Math.floor(Math.random() * 100),
-            })));
+            setTableData(
+                registrantsData.data.data.users
+                    .map((val, index) => ({
+                        sr_num: index + 1,
+                        name: val,
+                        rank: Math.floor(Math.random() * 50),
+                        marks: Math.floor(Math.random() * 50),
+                        progress: Math.floor(Math.random() * 100),
+                    }))
+                    .sort((val1, val2) => val1.progress - val2.progress)
+                    .map((val, index) => ({ ...val, sr_num: index + 1 })),
+            );
         }
-      }, [isRegistrantsSuccess]);
-
+    }, [isRegistrantsSuccess]);
+    const handleDropdownChange = (e) => {
+        const sortByval = e.target.value;
+        console.log(sortByval);
+        if (sortByval === SORT_TYPES.CHECKED_ASC) {
+            setTableData(
+                tableData
+                    .sort((val1, val2) => val1.progress - val2.progress)
+                    .map((val, index) => ({ ...val, sr_num: index + 1 })),
+            );
+        } else if (sortByval === SORT_TYPES.CHECKED_DES) {
+            setTableData(
+                tableData
+                    .sort((val1, val2) => val2.progress - val1.progress)
+                    .map((val, index) => ({ ...val, sr_num: index + 1 })),
+            );
+        } else if (sortByval === SORT_TYPES.ALPHA_ASC) {
+            setTableData(
+                tableData
+                    .sort((val1, val2) => val1.name.localeCompare(val2.name))
+                    .map((val, index) => ({ ...val, sr_num: index + 1 })),
+            );
+        } else if (sortByval === SORT_TYPES.ALPHA_DES) {
+            setTableData(
+                tableData
+                    .sort((val1, val2) => -val1.name.localeCompare(val2.name))
+                    .map((val, index) => ({ ...val, sr_num: index + 1 })),
+            );
+        } else if (sortByval === SORT_TYPES.RANKLIST) {
+            setTableData(
+                tableData
+                    .sort((val1, val2) => val1.rank - val2.rank)
+                    .map((val, index) => ({ ...val, sr_num: index + 1 })),
+            );
+        }
+        setSortBy(sortByval);
+    };
     if (isRegistrantsLoading) {
         return <div>Loading...</div>;
     }
@@ -31,19 +83,25 @@ const Bottom = () => {
                 <div>
                     Sort by :
                     {' '}
-                    <select className="dropdown">
-                        <option>Checked (0 - 100%)</option>
-                        <option>B</option>
+                    <select
+                      className="dropdown"
+                      onChange={handleDropdownChange}
+                      value={sortBy}
+                    >
+                        {Object.values(SORT_TYPES).map((val) => (<option key={val}>{val}</option>))}
                     </select>
                 </div>
                 <div className="cta-flex">
                     <SecondaryCTA text="Autocheck" />
-                    <PrimaryCTA text="Publish Results" additionalClassName="quiz-check-button" />
+                    <PrimaryCTA
+                      text="Publish Results"
+                      additionalClassName="quiz-check-button"
+                    />
                 </div>
             </div>
             <CheckingTable data={tableData} />
         </div>
-  );
+    );
 };
 
 export default Bottom;
