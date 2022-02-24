455d3a9880b684e5ce8476460045c799395a3833
Saurabh <saurabhsangam78@gmail.com>
2022-02-25 01:13:37 +0530

    add section fetching and switch view logic

--- src/api/quizzes/useSections.js
+++ src/api/quizzes/useSections.js
@@ -1,10 +1,15 @@
-import { useMutation, useQuery } from 'react-query';
+import { useMutation, useQueries, useQuery } from 'react-query';
 import * as fetchers from './sectionFetcher';
 
 export const useAddSection = () => useMutation(fetchers.addSectionToQuiz);
 
 export const useGetSectionDetails = (sectionId) => useQuery([`getSection-${sectionId}`, sectionId],
 fetchers.getSectionDetails, { enabled: !!sectionId });
+
+export const useGetMultipleSections = (sectionIds) => useQueries(sectionIds.map((id) => ({
+ queryKey: [`getSection-${id}`, id],
+ queryFn: fetchers.getSectionDetails,
+})));
 
 export const useUpdateSection = () => useMutation(fetchers.updateSectionDetails);
 
--- src/components/pages/GiveQuiz/SectionLanding.jsx
+++ src/components/pages/GiveQuiz/SectionLanding.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Redirect, useParams } from 'react-router-dom';
 import PrimaryCTA from '@components/Buttons/PrimaryCTA';
 import ModalWrapper from '@components/Modals/ModalWrapper';
 import SubmitQuiz from '@components/Modals/SubmitQuiz';
+import useGiveQuizStore from '@redux/store/zustand/giveQuiz';
+import _ from 'lodash';
 
 const SectionLanding = () => {
-    const { sectionId } = useParams();
-    console.log(sectionId);
+    const { quizId, sectionId } = useParams();
+    const { quiz, sections } = useGiveQuizStore();
+
     const [showModal, setShowModal] = React.useState(false);
+
+    if (!quiz.quizioID) return <Redirect to={`/quiz/${quizId}`} />;
+
+    const currentSection = _.find(sections, { quizioID: sectionId }) || {};
+    const { title, description } = currentSection;
 
     return (
         <>
-            <h1 className="text-3xl font-bold">Section Name</h1>
+            <h1 className="text-3xl font-bold">{title}</h1>
             <h2 className="mt-8 text-2xl font-semibold">Section Instructions</h2>
             <p className="text-grey-N6 mt-6">
-                Paper: There were two papers in JEE Advanced 2020 exam which are Paper
-                1 and Paper 2. Aspirants must attempt both the papers to qualify for the exam.
-                Duration of Exam: The exam will be conducted for 3 hours for each paper.
-                Type of questions: The JEE Advanced 2020 question paper has both multiple-choice
-                questions and numerical type questions.
-                Language: Based on the aspirants preference, they can choose the
-                language either English or Hindi anytime during the exam.
-                Subjects: The exam is conducted for three subjects which are Physics, Chemistry and Mathematics.
-                Shifts: Both the papers will be conducted on the same day which is 17th May 2020.
+                {description || 'No description provided'}
             </p>
             <div className="ml-auto mt-16 w-40">
                 <PrimaryCTA text="Start Answering" onClick={() => setShowModal(true)} />
