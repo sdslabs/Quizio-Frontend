6578bd270ef67781fdd41ece19cb38d348b760e2
Nishanth R <nishusheela2011@gmail.com>
2022-03-08 01:47:24 +0530

    adds preliminary question form template

--- src/components/pages/EditQuiz/MainForm/QuestionsMain.jsx
+++ src/components/pages/EditQuiz/MainForm/QuestionsMain.jsx
@@ -1,17 +1,129 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import useCreateQuizStore from '@redux/store/zustand/createQuiz';
 import TextField from '@components/Input/TextField';
 import MarkdownTextField from '@components/Input/MarkdownTextField';
 import PrimaryCTA from '@components/Buttons/PrimaryCTA';
 import { useUpdateSection } from '@api/quizzes/useSections';
 import _ from 'lodash';
-import { useAddQuestion } from '@api/quizzes/useQuestions';
+import { useAddQuestion, useUpdateQuestion } from '@api/quizzes/useQuestions';
+import SecondaryCTA from '@components/Buttons/SecondaryCTA';
+import RadioGroup from '@components/Input/RadioGroup';
+import Switch from 'react-switch';
 
 const Questions = () => {
     const { showQuestion } = useCreateQuizStore();
 
-    if (showQuestion) return <div>Question Creation Form</div>;
+    if (showQuestion) return <Question />;
     return <SectionDescription />;
+};
+
+const Question = () => {
+    const {
+        sections, questions, showQuestion, activeSectionIndex, activeQuestion,
+       } = useCreateQuizStore();
+
+       const choices = [
+        {
+          choice: 'JS',
+          quizioID: '1',
+        },
+        {
+          choice: 'C++',
+          quizioID: '2',
+        },
+      ];
+
+    const [selected, setSelected] = useState('');
+    const [autoCheck, setAutoCheck] = useState(false);
+    const [questionText, setQuestionText] = useState('');
+
+    console.log('show question', showQuestion);
+    console.log('show active question', activeQuestion);
+    const currentSection = sections[activeSectionIndex];
+    const currentQuestion = questions[activeQuestion];
+    console.log('question: ', currentQuestion);
+
+    // const setQuestion (value) => {};
+    const { isLoading, mutate: mutateQuestion, isSuccess } = useUpdateQuestion();
+    const isAddingQuestion = false;
+    const handleSave = () => {
+        mutateQuestion({ sectionId: currentQuestion.id, body: _.omit(currentSection, ['id', 'questions']) });
+    };
+    console.log(isSuccess);
+
+    const toggleSwitch = () => { setAutoCheck(!autoCheck); };
+
+    // const setAutoCheck = () => { console.log('autocheck'); };
+    return (
+        <div className="quiz-details">
+            <div className="quiz-details-title">{currentSection ? `${currentSection.title}` : ''}</div>
+            <div className="quiz-question">
+                <div className="question-title mt-6 mb-6">Question 1</div>
+
+                <MarkdownTextField
+                  id="question-description"
+                  val={questionText}
+                  limit={1500}
+                  placeholder="Enter question here"
+                  setVal={setQuestionText}
+                />
+                <div className="mcq-options ml-5">
+                    <RadioGroup
+                      choices={choices}
+                      selected={selected}
+                      setSelected={setSelected}
+                    />
+                    <div className="w-1/6 pb-6 pt-5">
+                        <SecondaryCTA text="+ Add Option" />
+                    </div>
+                </div>
+                <hr className="rounded" color="grey" />
+                <div className="question-marks inline-flex">
+                    <div className="pt-10 pr-4">
+                        Marks:
+                    </div>
+                    <div>
+                        <TextField
+                          id="question-marks"
+                          placeholder="0"
+                          // setVal={}
+                          // val={currentQuestion.marks}
+                        />
+                    </div>
+                    <div className="autocheck ml-5 pt-10 pr-4">
+                        Autocheck
+                        <Switch
+                          className="pl-5"
+                          onClick={toggleSwitch}
+                          onChange={toggleSwitch}
+                          checked={autoCheck}
+                          offColor="#DADADA"
+                          onColor="#604195"
+                          uncheckedIcon={false}
+                          checkedIcon={false}
+                          height={15}
+                          width={30}
+                        />
+                    </div>
+                    <div className="mcq-ans">
+                        Answer:
+                        <select
+                          className="border-2 border-grey"
+                        >
+                            {choices.map((val) => (
+                                <option key={val.quizioID}>
+                                    {val.choice}
+                                </option>
+))}
+                        </select>
+                    </div>
+                </div>
+                <div className="w-40 ml-auto mt-8">
+                    {isLoading || isAddingQuestion ? 'Saving...' : <PrimaryCTA text="Save Changes" onClick={handleSave} />}
+                </div>
+            </div>
+        </div>
+   );
 };
 
 const SectionDescription = () => {
@@ -19,7 +131,7 @@ const SectionDescription = () => {
  sections, activeSectionIndex, updateSection, addQuestionToSection, addQuestion, toggleQuestionForm,
 } = useCreateQuizStore();
     const currentSection = sections[activeSectionIndex];
-
+    console.log(activeSectionIndex);
     const setSectionTitle = (value) => updateSection({ ...currentSection, title: value });
     const setSectionDescription = (value) => updateSection({ ...currentSection, description: value });
 
