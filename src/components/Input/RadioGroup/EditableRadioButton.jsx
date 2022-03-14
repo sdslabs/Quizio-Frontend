02968740fc6113fcd55dc7d87c5f06352300ba29
Nishanth R <nishusheela2011@gmail.com>
2022-03-14 16:12:14 +0530

    [fix] fixes bugs wrt editable radio button

--- src/components/Input/RadioGroup/EditableRadioButton.jsx
+++ src/components/Input/RadioGroup/EditableRadioButton.jsx
@@ -6,7 +6,7 @@ import { ReactComponent as DeleteButton } from '@icons/delete_button.svg';
 import _ from 'lodash';
 
 const RadioButton = ({
- text, /* onChange, */ quizioID, choices, setChoices,
+ text, /* onChange, */ quizioID, choices, setChoices, setMcqCount, mcqCount,
 }) => {
     const [value, setValue] = useState(text);
     const handleChange = (e) => {
@@ -22,10 +22,12 @@ const RadioButton = ({
     };
     // setValue(value);
     const handleDelete = () => {
+        setMcqCount(mcqCount - 1);
         // setChoices(choices.pop(choices.find((choice) => choice.quizioID === quizioID)));
         const option = choices.find((element) => element.quizioID === quizioID);
         const index = choices.indexOf(option);
         const temp = _.remove(choices, (e) => e !== option);
+        // console.log(temp);
         setChoices(temp);
 
         // const toChange = choices.find((element) => element.quizioID < 2);
@@ -44,7 +46,7 @@ const RadioButton = ({
                   className="radio-button-input"
                 />
                 <label htmlFor={quizioID} className="radio-button-label">
-                    <input type="text" className="radio-button-text" value={value} onChange={handleChange} />
+                    <input type="text" id={quizioID} className="radio-button-text w-auto" value={value} onChange={handleChange} />
                 </label>
                 <button type="button" className="w-6 h-6 m-2" onClick={handleDelete}>
                     <DeleteButton />
@@ -60,6 +62,8 @@ RadioButton.propTypes = {
 //   checked: PropTypes.bool,
   setChoices: PropTypes.func,
   choices: PropTypes.arrayOf(PropTypes.shape({ quizioID: PropTypes.string, choice: PropTypes.string })),
+  mcqCount: PropTypes.number.isRequired,
+  setMcqCount: PropTypes.func.isRequired,
 };
 
 RadioButton.defaultProps = {
