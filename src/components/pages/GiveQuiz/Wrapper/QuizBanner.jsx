4467c25961f1a24251d8b971609d3e95bf17a588
CapriciousRebel <code.capriciousrebel@gmail.com>
2022-03-21 12:47:19 +0530

    [Refactor]: Remove redundant `QuizTimer` and use the common `Countdown` component

--- src/components/pages/GiveQuiz/Wrapper/QuizBanner.jsx
+++ src/components/pages/GiveQuiz/Wrapper/QuizBanner.jsx
@@ -2,19 +2,25 @@ import React from 'react';
 import TimerIcon from '@icons/timerIcon.svg';
 import QuestionBubble from '@components/Visual/QuestionBubble';
 import useGiveQuizStore from '@redux/store/zustand/giveQuiz';
-import QuizTimer from './QuizTimer';
+import Countdown from '@components/Misc/Countdown';
 
 const QuizBanner = () => {
   const {
+    quiz,
     answeredQuestions,
     markedQuestions,
     markedAnsweredQuestions,
     totalQuestions,
   } = useGiveQuizStore();
+
   const QuestionBubbles = [
     {
       type: 'unanswered',
-      number: totalQuestions - (markedQuestions?.length || 0) - (answeredQuestions?.length || 0) - (markedAnsweredQuestions?.length || 0),
+      number:
+        totalQuestions
+        - (markedQuestions?.length || 0)
+        - (answeredQuestions?.length || 0)
+        - (markedAnsweredQuestions?.length || 0),
       label: 'Unanswered',
     },
     {
@@ -33,6 +39,7 @@ const QuizBanner = () => {
       label: 'Answered & Marked for review',
     },
   ];
+
   return (
       <div className="border-b border-grey-N4 pl-10 flex items-stretch">
           <div className="flex flex-wrap flex-grow justify-center disable-hover">
@@ -41,16 +48,16 @@ const QuizBanner = () => {
                       <QuestionBubble key={type} type={type} number={number} />
                       <p className="ml-4 mr-10 text-sm">{label}</p>
                   </div>
-      ))}
+        ))}
           </div>
           <div className="py-4 px-8 bg-purple-V1 flex items-center">
               <img src={TimerIcon} alt="" className="mr-2" />
               <p className="text-purple-V6 whitespace-nowrap">
-                  <QuizTimer />
+                  <Countdown time={quiz?.endTime} />
               </p>
           </div>
       </div>
-);
+  );
 };
 
 export default QuizBanner;
