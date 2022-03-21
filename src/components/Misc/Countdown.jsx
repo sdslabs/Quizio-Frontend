2d967de1f89f8ca1addefdc83c8fd434e28942cb
CapriciousRebel <code.capriciousrebel@gmail.com>
2022-03-21 12:53:20 +0530

    [Refactor]: Add default prop to countdown timer and eslint

--- src/components/Misc/Countdown.jsx
+++ src/components/Misc/Countdown.jsx
@@ -3,45 +3,47 @@ import PropTypes from 'prop-types';
 
 // Make sure time - now < 24hrs
 const Countdown = ({ time }) => {
-    const [countHours, setCountHours] = useState('00');
-    const [countMinutes, setCountMinutes] = useState('00');
-    const [countSeconds, setCountSecounds] = useState('00');
+  const period = useRef();
 
-    const period = useRef();
-    const startCountdownTimer = () => {
-        const endTime = new Date(time).getTime();
-        const now = new Date().getTime();
+  const [countHours, setCountHours] = useState('00');
+  const [countMinutes, setCountMinutes] = useState('00');
+  const [countSeconds, setCountSecounds] = useState('00');
 
-        period.current = setInterval(() => {
-            const duration = endTime - now;
-            const seconds = Math.floor((duration / 1000) % 60);
-            const minutes = Math.floor((duration / 1000 / 60) % 60);
-            const hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+  const startCountdownTimer = () => {
+    // TODO: use dayjs
+    const endTime = new Date(time).getTime();
+    const now = new Date().getTime();
 
-            if (duration < 0) {
-                clearInterval(period.current);
-            } else {
-                setCountHours(hours);
-                setCountMinutes(minutes);
-                setCountSecounds(seconds);
-            }
-        }, 1000);
-    };
-    useEffect(() => {
-        startCountdownTimer();
-        return () => {
-            clearInterval(period.current);
-        };
-    });
-    return (
-        <span>
-            {`${countHours} : ${countMinutes} : ${countSeconds}`}
-        </span>
-    );
+    period.current = setInterval(() => {
+      const duration = endTime - now;
+      const seconds = Math.floor((duration / 1000) % 60);
+      const minutes = Math.floor((duration / 1000 / 60) % 60);
+      const hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+      if (duration < 0) {
+        clearInterval(period.current);
+      } else {
+        setCountHours(hours);
+        setCountMinutes(minutes);
+        setCountSecounds(seconds);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    startCountdownTimer();
+    return () => clearInterval(period.current);
+  });
+
+  return <span>{`${countHours} : ${countMinutes} : ${countSeconds}`}</span>;
 };
 
 Countdown.propTypes = {
-    time: PropTypes.string.isRequired,
+  time: PropTypes.string,
+};
+
+Countdown.defaultProps = {
+  time: '0',
 };
 
 export default Countdown;
--- src/components/Modals/SubmitQuiz.jsx
+++ src/components/Modals/SubmitQuiz.jsx
@@ -11,141 +11,164 @@ import { PieChart } from 'react-minimal-pie-chart';
 import useGiveQuizStore from '@redux/store/zustand/giveQuiz';
 import { useSubmitQuiz } from '@api/quizzes/useQuizzes';
 import { useParams } from 'react-router';
+import log from '@utils/log';
 
 const Banner = ({ endTime, submitted }) => {
-    if (submitted || new Date(endTime) < new Date()) {
-        return (
-            <div className="py-6 px-8 bg-green-1 bg-opacity-10 rounded flex">
-                <img src={timerGreen} alt="" className="h-14 w-14 mr-6" />
-                <div>
-                    <p className="text-xl text-green-1 font-semibold opacity-90">
-                        {submitted ? 'Yay!' : 'Time’s up!'}
-                    </p>
-                    <p className="text-green-1 mt-1">
-                        Your quiz has been
-                        {' '}
-                        <span className="font-semibold">successfully submitted</span>
-                    </p>
-                </div>
-            </div>
-            );
-        }
-
+  if (submitted || new Date(endTime) < new Date()) {
     return (
-        <div className="py-6 px-8 bg-purple-V1 rounded flex">
-            <img src={timerIcon} alt="" className="h-14 w-14 mr-6" />
+        <div className="py-6 px-8 bg-green-1 bg-opacity-10 rounded flex">
+            <img src={timerGreen} alt="" className="h-14 w-14 mr-6" />
             <div>
-                <p className="text-xl text-purple font-semibold opacity-90">
-                    You still have
-                    {' '}
-                    <Countdown time={endTime} />
-                    {' '}
-                    left
+                <p className="text-xl text-green-1 font-semibold opacity-90">
+                    {submitted ? 'Yay!' : 'Time’s up!'}
                 </p>
-                <p className="text-purple mt-1">Are you sure you want to submit ?</p>
+                <p className="text-green-1 mt-1">
+                    Your quiz has been
+                    {' '}
+                    <span className="font-semibold">successfully submitted</span>
+                </p>
             </div>
         </div>
     );
+  }
+
+  return (
+      <div className="py-6 px-8 bg-purple-V1 rounded flex">
+          <img src={timerIcon} alt="" className="h-14 w-14 mr-6" />
+          <div>
+              <p className="text-xl text-purple font-semibold opacity-90">
+                  You still have
+                  {' '}
+                  <Countdown time={endTime} />
+                  {' '}
+                  left
+              </p>
+              <p className="text-purple mt-1">Are you sure you want to submit ?</p>
+          </div>
+      </div>
+  );
 };
 
 const SubmitQuiz = ({ setShowModal }) => {
-    const {
- totalQuestions, answeredQuestions, markedQuestions, markedAnsweredQuestions, quiz,
-} = useGiveQuizStore();
-const { quizID } = useParams();
-const {
-    mutate: submitQuiz,
-    isSuccess: submitSucess,
-  } = useSubmitQuiz();
+  const {
+    totalQuestions,
+    answeredQuestions,
+    markedQuestions,
+    markedAnsweredQuestions,
+    quiz,
+  } = useGiveQuizStore();
+  const { quizID } = useParams();
+  const { mutate: submitQuiz, isSuccess: submitSucess } = useSubmitQuiz();
 
   useEffect(() => {
     if (submitSucess) {
-        console.log('Quiz submitted');
-        // TO DO : redirect to required page
+      log('Quiz submitted, TODO : redirect to required page');
+      // TODO : redirect to required page
     }
   }, [submitSucess]);
 
-const handleSubmit = () => {
-    console.log(quizID, 'lll');
+  const handleSubmit = () => {
+    log('Submit Quiz!', { quizID });
     submitQuiz({ quizID });
-};
-const data = [
+  };
+
+  const data = [
     {
-        title: 'Not visited',
-        value: totalQuestions - (answeredQuestions?.length || 0) - (markedQuestions?.length || 0) - (markedAnsweredQuestions?.length || 0),
-        color: '#AD9EC9',
+      title: 'Not visited',
+      value:
+        totalQuestions
+        - (answeredQuestions?.length || 0)
+        - (markedQuestions?.length || 0)
+        - (markedAnsweredQuestions?.length || 0),
+      color: '#AD9EC9',
     },
     {
-        title: 'Answered questions',
-        value: answeredQuestions?.length || 0,
-        color: '#27A624',
+      title: 'Answered questions',
+      value: answeredQuestions?.length || 0,
+      color: '#27A624',
     },
     {
-        title: 'Marked for review',
-        value: markedQuestions?.length || 0,
-        color: '#FF8900',
+      title: 'Marked for review',
+      value: markedQuestions?.length || 0,
+      color: '#FF8900',
     },
     {
-        title: 'Answered and marked for review',
-        value: markedAnsweredQuestions?.length || 0,
-        color: '#604195',
+      title: 'Answered and marked for review',
+      value: markedAnsweredQuestions?.length || 0,
+      color: '#604195',
     },
-];
-    return (
-        <div className="py-6 px-8">
-            <div className="flex justify-between items-center mb-6">
-                <h1 className="text-lg font-semibold">Submit Quiz</h1>
-                <CrossIcon className="cursor-pointer" onClick={() => { setShowModal(false); }} />
-            </div>
-            <Banner endTime={quiz?.endTime} />
-            <div className="flex mt-6 items-center">
-                <div className="w-6/12">
-                    <div className="w-52 mx-auto">
-                        <PieChart
-                          data={data}
-                          radius={PieChart.defaultProps.radius - 2}
-                          segmentsShift={1}
-                        />
-                    </div>
-                </div>
-                <div className="flex-grow">
-                    <p className="text-lg mb-6">
-                        Total number of questions:
-                        {' '}
-                        <span className="font-semibold">{totalQuestions}</span>
-                    </p>
-                    <div>
-                        {data.map(({ title, value, color }) => (
-                            <div key={title} className="flex my-3">
-                                <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <rect width="19" height="19" rx="4" fill={color} />
-                                </svg>
-                                <p className="text-sm ml-3">
-                                    {title}
-                                    :
-                                    {' '}
-                                    <span className="font-semibold">{value}</span>
-                                </p>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-            <div className="flex justify-end mt-10">
-                <div className="w-24">
-                    <SecondaryCTA text="Cancel" onClick={() => { setShowModal(false); }} />
-                </div>
-                <div className="w-24 ml-4">
-                    <PrimaryCTA text="Submit" onClick={handleSubmit} />
-                </div>
-            </div>
-        </div>
-    );
+  ];
+  return (
+      <div className="py-6 px-8">
+          <div className="flex justify-between items-center mb-6">
+              <h1 className="text-lg font-semibold">Submit Quiz</h1>
+              <CrossIcon
+                className="cursor-pointer"
+                onClick={() => {
+            setShowModal(false);
+          }}
+              />
+          </div>
+          <Banner endTime={quiz?.endTime} />
+          <div className="flex mt-6 items-center">
+              <div className="w-6/12">
+                  <div className="w-52 mx-auto">
+                      <PieChart
+                        data={data}
+                        radius={PieChart.defaultProps.radius - 2}
+                        segmentsShift={1}
+                      />
+                  </div>
+              </div>
+              <div className="flex-grow">
+                  <p className="text-lg mb-6">
+                      Total number of questions:
+                      {' '}
+                      <span className="font-semibold">{totalQuestions}</span>
+                  </p>
+                  <div>
+                      {data.map(({ title, value, color }) => (
+                          <div key={title} className="flex my-3">
+                              <svg
+                                width="19"
+                                height="19"
+                                viewBox="0 0 19 19"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                  <rect width="19" height="19" rx="4" fill={color} />
+                              </svg>
+                              <p className="text-sm ml-3">
+                                  {title}
+                                  :
+                                  {' '}
+                                  <span className="font-semibold">{value}</span>
+                              </p>
+                          </div>
+            ))}
+                  </div>
+              </div>
+          </div>
+          <div className="flex justify-end mt-10">
+              <div className="w-24">
+                  <SecondaryCTA
+                    text="Cancel"
+                    onClick={() => {
+              setShowModal(false);
+            }}
+                  />
+              </div>
+              <div className="w-24 ml-4">
+                  <PrimaryCTA text="Submit" onClick={handleSubmit} />
+              </div>
+          </div>
+      </div>
+  );
 };
 
 Banner.propTypes = {
-    endTime: PropTypes.string.isRequired,
-    submitted: PropTypes.bool.isRequired,
+  endTime: PropTypes.string.isRequired,
+  submitted: PropTypes.bool.isRequired,
 };
 
 export default SubmitQuiz;
