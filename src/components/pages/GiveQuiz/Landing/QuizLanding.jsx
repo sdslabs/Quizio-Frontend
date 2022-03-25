e3a6f3c85197b1485c0d322f8a4834da633b49d1
CapriciousRebel <code.capriciousrebel@gmail.com>
2022-03-25 16:46:56 +0530

    [Feature]: Show submit quiz prompt on last question

--- src/components/pages/GiveQuiz/Landing/QuizLanding.jsx
+++ src/components/pages/GiveQuiz/Landing/QuizLanding.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+/* eslint-disable react/no-children-prop */
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useParams, useHistory } from 'react-router-dom';
+import { useHistory, useLocation, useParams } from 'react-router-dom';
 import useGiveQuizStore from '@redux/store/zustand/giveQuiz';
 import { useGetQuiz } from '@api/quizzes/useQuizzes';
 import { useGetResponseStatus } from '@api/quizzes/useResponse';
@@ -8,9 +9,18 @@ import PrimaryCTA from '@components/Buttons/PrimaryCTA';
 import log from '@utils/log';
 import Fetching from '@components/Misc/Fetching';
 import ReactMarkdown from 'react-markdown';
+import ModalWrapper from '@components/Modals/ModalWrapper';
+import SubmitQuiz from '@components/Modals/SubmitQuiz';
 
+function useQuery() {
+  const { search } = useLocation();
+
+  return React.useMemo(() => new URLSearchParams(search), [search]);
+}
 const QuizLanding = () => {
+  const query = useQuery();
   const history = useHistory();
+  const [showModal, setShowModal] = useState(false);
   const { quizID } = useParams();
   const userID = useSelector((state) => state.auth.user.userID);
 
@@ -38,6 +48,13 @@ const QuizLanding = () => {
   const handleContinue = () => {
     history.push(`/quiz/attempt/${quizID}/${quizData?.quiz?.sections[0]}`);
   };
+
+  useEffect(() => {
+    log('QuizLanding', query.get('submit') === 'true');
+    if (query.get('submit') === 'true') {
+      setShowModal(true);
+    }
+  }, [query]);
 
   // handle response status
   useEffect(() => {
@@ -89,19 +106,31 @@ const QuizLanding = () => {
 
           <div className="mt-6">
               {/* eslint-disable-next-line react/no-children-prop */}
-              <ReactMarkdown children={quizData?.quiz?.description || 'No description available'} />
+              <ReactMarkdown
+                children={quizData?.quiz?.description || 'No description available'}
+              />
           </div>
           <h2 className="mt-8 text-2xl font-semibold">Instructions</h2>
           {/* <p className="text-grey-N6 mt-6">
               {quizData?.quiz?.instructions || 'No instructions available'}
           </p> */}
           <div className="mt-6">
-              {/* eslint-disable-next-line react/no-children-prop */}
-              <ReactMarkdown children={quizData?.quiz?.instructions || 'No instructions available'} />
+              <ReactMarkdown
+                children={quizData?.quiz?.instructions || 'No instructions available'}
+              />
           </div>
           <div className="ml-auto mt-16 w-28">
               <PrimaryCTA text="Continue" onClick={handleContinue} />
           </div>
+          {showModal && (
+          <ModalWrapper
+            showModal={showModal}
+            setShowModal={setShowModal}
+            hideOnOverlayClick
+          >
+              <SubmitQuiz setShowModal={setShowModal} />
+          </ModalWrapper>
+      )}
       </>
   );
 };
