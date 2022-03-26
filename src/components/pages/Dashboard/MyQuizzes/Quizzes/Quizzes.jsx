f6b93383ddf0368b266c49d904a1dc705b4b8e2b
CapriciousRebel <code.capriciousrebel@gmail.com>
2022-03-26 18:20:36 +0530

    [Fix]: filter quizzes based on serverTime on Dashboard

--- src/components/pages/Dashboard/MyQuizzes/Quizzes/Quizzes.jsx
+++ src/components/pages/Dashboard/MyQuizzes/Quizzes/Quizzes.jsx
@@ -1,45 +1,52 @@
 import React, { useEffect, useState } from 'react';
-// import { getAllQuizzes } from '@api/quizzes/quizzesFetcher';
-import { useGetAllQuizzes } from '@api/quizzes/useQuizzes';
-import log from '@utils/log';
 import dayjs from 'dayjs';
+import { useGetAllQuizzes } from '@api/quizzes/useQuizzes';
+import { useGetCurrentServerTime } from '@api/misc/useTime';
+import log from '@utils/log';
 import QuizCard from './QuizCard';
 import '@pagestyles/dashboard/quizzes.scss';
 
 const Quizzes = () => {
   const {
- data, isFetching, isSuccess, isRefetching,
-} = useGetAllQuizzes();
+    data: quizzesData,
+    isFetching: isQuizzesFetching,
+    isSuccess: isQuizzesSuccess,
+    isRefetching: isQuizzesRefetching,
+  } = useGetAllQuizzes();
+
+  const {
+    data: serverTimeData,
+    isSuccess: isServerTimeSuccess,
+  } = useGetCurrentServerTime();
 
   const [onGoingQuizzes, setOnGoingQuizzes] = useState(null);
   const [upComingQuizzes, setUpcomingQuizzes] = useState(null);
-  const [offset, setOffset] = React.useState(0);
 
   useEffect(async () => {
-    if (isSuccess) {
-      log({ quizData: data });
-      const timeOffset = dayjs() - dayjs(data.data.data.serverTime);
-      setOffset(timeOffset);
-    }
+    if (isQuizzesSuccess && isServerTimeSuccess) {
+      const serverTime = dayjs(serverTimeData?.data?.data?.serverTime);
 
-    const upcomingFilter = (quiz) => {
-      if (quiz.startTime) {
-        const quizStartTime = new Date(quiz.startTime);
-        return quizStartTime > dayjs() - offset;
-      }
-      return true;
-    };
-    setUpcomingQuizzes(
-      data?.data?.quizzes?.filter(
-        (quiz) => quiz.startTime && upcomingFilter(quiz),
-      ),
-    );
-    setOnGoingQuizzes(
-      data?.data?.quizzes?.filter(
-        (quiz) => quiz.startTime && !upcomingFilter(quiz),
-      ),
-    );
-  }, [isSuccess]);
+      const upcomingFilter = (quiz) => {
+        if (quiz.startTime) {
+          const quizStartTime = dayjs(quiz.startTime);
+          return quizStartTime > serverTime;
+        }
+        return true;
+      };
+
+      setUpcomingQuizzes(
+        quizzesData?.data?.quizzes?.filter(
+          (quiz) => quiz.startTime && upcomingFilter(quiz),
+        ),
+      );
+
+      setOnGoingQuizzes(
+        quizzesData?.data?.quizzes?.filter(
+          (quiz) => quiz.startTime && !upcomingFilter(quiz),
+        ),
+      );
+    }
+  }, [isQuizzesSuccess, isServerTimeSuccess]);
 
   useEffect(() => {
     log({ onGoingQuizzes }, null, false);
@@ -54,7 +61,7 @@ const Quizzes = () => {
           <div className="ongoing-quizzes">
               <div className="title">Ongoing Quizzes</div>
               <div className="quiz-list hide-scrollbar">
-                  {isFetching && !isRefetching && (
+                  {isQuizzesFetching && !isQuizzesRefetching && (
                   <div>Fetching ongoing quizzes...</div>
           )}
                   {onGoingQuizzes && onGoingQuizzes.length !== 0 ? (
@@ -72,7 +79,7 @@ const Quizzes = () => {
           <div className="upcoming-quizzes">
               <div className="title">Upcoming Quizzes</div>
               <div className="quiz-list hide-scrollbar">
-                  {isFetching && !isRefetching && (
+                  {isQuizzesFetching && !isQuizzesRefetching && (
                   <div>Fetching upcoming quizzes...</div>
           )}
                   {upComingQuizzes && upComingQuizzes.length !== 0 ? (
