4667ba5103152e514a97d67f8fe050601137cf18
Angad Kambli <angadkambli@gmail.com>
2022-03-27 23:52:49 +0530

    fix subjective caching issue

--- src/api/quizzes/useResponse.js
+++ src/api/quizzes/useResponse.js
@@ -1,8 +1,12 @@
 import { useQuery, useMutation } from 'react-query';
 import * as fetchers from './responseFetcher';
 
-export const useGetResponse = (userID, questionID) => useQuery(['getResponse', userID, questionID], fetchers.getResponse);
+export const useGetResponse = (userID, questionID) => useQuery(['getResponse', userID, questionID], fetchers.getResponse, {
+  retry: 0,
+});
 
-export const useGetResponseStatus = (userID, quizID) => useQuery(['getResponse', userID, quizID], fetchers.getResponseStatus);
+export const useGetResponseStatus = (userID, quizID) => useQuery(['getResponse', userID, quizID], fetchers.getResponseStatus, {
+  retry: 0,
+});
 
 export const useUpdateResponse = () => useMutation(fetchers.updateResponse);
