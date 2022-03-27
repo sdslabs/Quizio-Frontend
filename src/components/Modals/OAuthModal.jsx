bad8aa7f1c56b05c3a3b7d3fef9c8d3e01235dc2
CapriciousRebel <code.capriciousrebel@gmail.com>
2022-03-28 01:14:56 +0530

    [Feature]: Add conditional rendering for continue with google and github, use is production variable

--- src/components/Modals/OAuthModal.jsx
+++ src/components/Modals/OAuthModal.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import { ReactComponent as CrossIcon } from '@icons/cross.svg';
-// import LoginWithGithub from '@components/Buttons/LoginWithGithub';
-import LoginWithGoogle from '@components/Buttons/LoginWithGoogle';
 import { PropTypes } from 'prop-types';
+import { ReactComponent as CrossIcon } from '@icons/cross.svg';
+import LoginWithGithub from '@components/Buttons/LoginWithGithub';
+import LoginWithGoogle from '@components/Buttons/LoginWithGoogle';
+import { isProduction } from '@config/config';
 
 const OAuthModal = ({ setShowModal }) => (
     <div className="">
         <div className="flex justify-between p-10 pb-4 pt-8">
             <div className="text-2xl text-purple-V6 font-bold">Join Us!</div>
-            <CrossIcon className="m-2 cursor-pointer" onClick={() => { setShowModal(false); }} />
+            <CrossIcon
+              className="m-2 cursor-pointer"
+              onClick={() => {
+          setShowModal(false);
+        }}
+            />
         </div>
-        <div className="font-normal text-m pl-10 pr-10">To begin this journey, tell us via which platform you want to log in !</div>
+        <div className="font-normal text-m pl-10 pr-10">
+            To begin this journey, tell us via which platform you want to log in !
+        </div>
         <div className="">
-            {/* <LoginWithGithub /> */}
+            {!isProduction && <LoginWithGithub />}
             <LoginWithGoogle />
         </div>
     </div>
 );
 
 OAuthModal.propTypes = {
-    setShowModal: PropTypes.func.isRequired,
+  setShowModal: PropTypes.func.isRequired,
 };
 
 export default OAuthModal;
