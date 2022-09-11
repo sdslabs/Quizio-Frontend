3b367b0e5d96e1f98636ed9c6aa6f88c184c9c0e
Pradnya <pradnyashimpi22@gmail.com>
2022-09-11 17:33:08 +0530

    Remove log

--- src/components/pages/CheckQuiz/ResponseList/Bottom/index.jsx
+++ src/components/pages/CheckQuiz/ResponseList/Bottom/index.jsx
@@ -43,7 +43,6 @@ const Bottom = () => {
 
   useEffect(() => {
     if (isRegistrantsSuccess) {
-      log('hi');
       log({ registrantsData });
       setTableData(
         registrantsData.data.data.rankList
